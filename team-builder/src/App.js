2e60111de71700b04793d5d7f8d0db06439d0929
Alison Ludick <alison.ludick@gmail.com>
2019-09-18 13:58:20 +0200

    initial commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Class List 2019:</h1>
+    </div>
+  );
+}
+
+export default App;
